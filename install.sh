7118b672cf56a1f33f73d494df83d35d801fc9f5
aburayyanjeffry <linuxlah@gmail.com>
2020-01-15 09:52:33 +0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+# Author: Jeffry Johar
+# Date: 1/15/2020
+# About: bb installer
+
+#OS=$(uname -o)
+OS="Android"
+
+case $OS in
+	"GNU/Linux")
+	  echo "Linux rocks"
+	  echo "cp bb /usr/local/bin"
+	  cp bb /usr/local/bin
+	  ;;
+
+	 "Android")
+           echo "Android rocks"
+           if [[ -f "/data/data/com.termux/files/usr/bin/" ]]
+	   then
+	     echo "cp bb /data/data/com.termux/files/usr/bin/"
+           else
+             echo "termux dir not found."
+             echo "copy bb to $HOME and use it as \"./bb\" "
+           fi	  
+	 ;;
+esac	 
